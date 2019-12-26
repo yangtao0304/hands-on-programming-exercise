e9deb5f50419d652dc9baacb94488408f913fc79
Yang Tao <im.yangtao0304@gmail.com>
2019-12-26 09:41:29 +0800

    feat: update 032

--- HOT_100/032_longest_valid_parenthese.py
+++ HOT_100/032_longest_valid_parenthese.py
@@ -24,7 +24,41 @@ class Solution:
 
         return max_len
 
+    # dp
+    # if s[i]==')' and s[i-1]=='(':
+    # dp[i] = dp[i-2] + 2
+    # if s[i] == s[i-1] == ')' and s[i-dp[i-1]-1]=='(':
+    # dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
+    def longestValidParentheses2(self, s: str) -> int:
+        max_len = 0
+        dp = [0 for _ in range(len(s)+1)]
+        for i in range(2, len(s)+1):
+            if s[i-1] == ')' and s[i-2] == '(':
+                dp[i] = dp[i-2]+2
+            elif s[i-1] == s[i-2] == ')' and i-1-dp[i-1]-1 >= 0 and s[i-1-dp[i-1]-1] == '(':
+                dp[i] = dp[i-1]+2+dp[i-dp[i-1]-2]
+            max_len = max(max_len, dp[i])
+
+        import numpy as np
+        print(np.array(dp))
+        return max_len
+
+    def longestValidParentheses2_revised(self, s: str) -> int:
+        max_len = 0
+        dp = [0 for _ in range(len(s))]
+        for i in range(1, len(s)):
+            if s[i] == ')':
+                if s[i-1] == '(':
+                    dp[i] = (dp[i-2] if i-2 >= 0 else 0) + 2
+                elif s[i-1] == ')' and i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1]=='(':
+                    dp[i] = dp[i-1] + 2 + (dp[i-dp[i-1]-2] if i-dp[i-1]-2>=0 else 0)
+                max_len = max(max_len, dp[i])
+
+        import numpy as np
+        print(np.array(dp))
+        return max_len
+
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.longestValidParentheses('(()'))
+    print(s.longestValidParentheses2_revised("(()))())("))
