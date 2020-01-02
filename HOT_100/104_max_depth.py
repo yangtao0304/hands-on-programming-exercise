d80b702f081125b960c36e89d32b4796871dc5a5
Yang Tao <im.yangtao0304@gmail.com>
2020-01-02 08:44:31 +0800

    feat: modify 104

--- HOT_100/104_max_depth.py
+++ HOT_100/104_max_depth.py
@@ -40,9 +40,9 @@ class Solution:
         stack = [(root, 1)]
         while stack:
             cur_node, cur_depth = stack.pop()
-            if cur_node:
-                depth = max(depth, cur_depth)
+            depth = max(depth, cur_depth)
+            if cur_node.left:
                 stack.append((cur_node.left, cur_depth+1))
+            if cur_node.right:
                 stack.append((cur_node.right, cur_depth+1))
-
         return depth
